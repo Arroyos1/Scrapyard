238ab7c4e4c96967c006d3a24dc2aa6ef0014233
BrianA <bdalcorn@outlook.com>
2016-07-29 09:34:21 +0200

    Player1 Spawns of f key ScrumMaster Script Created

---
+++ Scrapyard/Assets/Scripts/ScrumMaster.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine . UI;
+
+
+public class ScrumMaster : MonoBehaviour
+{
+     public GameObject scrumbag;
+     public GameObject spawnPlayer1;
+     public GameObject spawnPlayer2;
+
+     Vector3 startPlayer1; 
+     Vector3 startPlayer2;
+     Quaternion spawnRotation = Quaternion . identity;
+
+	// Use this for initialization
+	void Start ()
+     {
+          Vector3 startPlayer2 = spawnPlayer2 . transform . position;
+     }
+	
+	// Update is called once per frame
+	void Update ()
+     {
+         if ( Input . GetKeyDown ( "f" ) )
+               {
+               SpawnPlayer1 ( );
+               }
+
+     }
+
+   void SpawnPlayer1()
+     {
+          Vector3 startPlayer1 = spawnPlayer1 . transform . position;
+          Debug . Log ( "Vector3 startPlayer1 = " + startPlayer1 );
+          Instantiate ( scrumbag , startPlayer1 , spawnRotation );
+     }
+
+         
+}
